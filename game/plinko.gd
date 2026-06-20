4e7960720dbea9393ee55936e3deb0101aa9edc8
Claire Jane <claireejane05@gmail.com>
2026-06-20 10:11:16 -0700

    added plinko bouncers and better organization for them

--- game/plinko.gd
+++ game/plinko.gd
@@ -1,9 +1,21 @@
 extends Node2D
-
+@export var bouncers: Array[PackedScene] 
+@onready var points: Node2D = $Points
+@onready var boings: Node2D = $Board/Boing
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var spots = points.get_children()
+	var i = 0
+	
+	for spot in spots:
+		if i%3:
+			i += 1
+		else: i = 1	
+		var scene = bouncers[i]
+		var bouncer = scene.instantiate()
+		boings.add_child(bouncer)
+		bouncer.global_position = spot.global_position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
