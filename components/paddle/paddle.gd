b3ce500fdb2895b711f505946fb5feede95a80dc
claireejane <claireejane05@gmail.com>
2026-06-21 01:08:23 -0700

    some bullshit

--- components/paddle/paddle.gd
+++ components/paddle/paddle.gd
@@ -17,7 +17,7 @@ func _physics_process(delta: float) -> void:
 		_follow_input()
 	
 	move_and_collide(velocity * delta)
-	if Input.is_action_just_pressed("left_switch_ball") and player == "left":
+	if Input.is_action_just_pressed("left_switch_stick") and player == "left":
 		_switch_to_next()
 
 
