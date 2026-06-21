5f75efc3dbc33feaa8a8b212c6413289db6c3748
claire61 <claire@blastworksinc.ca>
2026-06-20 19:07:29 -0700

    unbroke the thing i broke

--- components/paddle/paddle.gd
+++ components/paddle/paddle.gd
@@ -1,39 +1,25 @@
-extends CharacterBody2D
 class_name Paddle
-@export var states: Array[State] = []
-var state_name_dict: Dictionary[String,State] = {}
-var active_state: State = null
-
+extends CharacterBody2D
+@export var player: String 
 
 const MOVE_SPEED: float = 300
 
 var input: float = 0.0
 var target: Node2D = null
 
-func _ready() -> void:
-	for state in states: 
-		state.paddle = self
-		state_name_dict[state.name] = state
-	active_state = states[0]
-	active_state.enter()
-	
-func switch_state(state_name: String):
-	active_state = states.filter(func(s): return s.name == state_name)[0]
+
 # If a target is set (i.e. AI paddle), follow it
 # Otherwise rely on manual input
 func _physics_process(delta: float) -> void:
-	if active_state.has_method("process"):
-		active_state.process(delta)
 	if target:
 		_follow_target()
 	else:
 		_follow_input()
 	
 	move_and_collide(velocity * delta)
-	
-func _unhandled_input(event: InputEvent) -> void:
-	if active_state.has_method("input"):
-		active_state.input(event)
+	if Input.is_action_just_pressed("left_switch_ball") and player == "left":
+		_switch_to_next()
+
 
 # Update velocity based on input
 func _follow_input() -> void:
@@ -48,3 +34,6 @@ func _follow_target() -> void:
 	var diff := target.global_position.y - global_position.y
 	var direction := clampf(diff, -1.0, 1.0)
 	velocity.y = direction * MOVE_SPEED
+
+func _switch_to_next()-> void:
+	pass
