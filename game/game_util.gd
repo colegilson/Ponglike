8918c8da635922d4e7554934c9a10b494a3d4726
claireejane <claireejane05@gmail.com>
2026-06-21 14:57:20 -0700

    gameutil

---
+++ game/game_util.gd
@@ -0,0 +1,8 @@
+class_name GameUtility
+
+static func get_game() -> Game:
+	var scene_tree := Engine.get_main_loop() as SceneTree
+	var game := scene_tree.current_scene as Game
+	assert(game, "Trying to get game at an invalid time!")
+	return game #should serve to give us the main root node at any time
+	 
