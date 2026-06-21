beffc790d2406b4ca761994d7bb0ecc40917c339
claireejane <claireejane05@gmail.com>
2026-06-21 14:18:43 -0700

    starting store bomboclaat

--- components/ball/ball_data.gd
+++ components/ball/ball_data.gd
@@ -5,6 +5,9 @@ extends Resource
 
 @export var name: String
 @export var bounce_coefficient: float
+@export var description: String
+@export var particle_trail: AnimatedTexture
+
 @export var return_speed: float
 @export var superpower_scene: PackedScene
 @export var basic_scene: PackedScene
--- components/stick_data.gd
+++ components/stick_data.gd
@@ -2,8 +2,11 @@ class_name StickData
 extends Resource
 
 @export var name: String
+@export var description: String
 @export var bounce_coefficient: float
 @export var return_speed: float
 @export var superpower_scene: PackedScene
 @export var basic_scene: PackedScene
 @export var sprite: Texture2D
+@export var particle_trail: AnimatedTexture
+@export var sfx: AudioStream
