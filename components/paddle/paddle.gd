d8683fe805f80b9dbcaeb5a133ffffcf164f861d
claire61 <claire@blastworksinc.ca>
2026-06-20 18:31:38 -0700

    rewrote state machine class - resources for states, each characterbody is a state manager and passes inputs down and delegates code down

--- components/paddle/paddle.gd
+++ components/paddle/paddle.gd
@@ -1,22 +1,39 @@
-class_name Paddle
 extends CharacterBody2D
+class_name Paddle
+@export var states: Array[State] = []
+var state_name_dict: Dictionary[String,State] = {}
+var active_state: State = null
+
 
 const MOVE_SPEED: float = 300
 
 var input: float = 0.0
 var target: Node2D = null
 
-
+func _ready() -> void:
+	for state in states: 
+		state.paddle = self
+		state_name_dict[state.name] = state
+	active_state = states[0]
+	active_state.enter()
+	
+func switch_state(state_name: String):
+	active_state = states.filter(func(s): return s.name == state_name)[0]
 # If a target is set (i.e. AI paddle), follow it
 # Otherwise rely on manual input
 func _physics_process(delta: float) -> void:
+	if active_state.has_method("process"):
+		active_state.process(delta)
 	if target:
 		_follow_target()
 	else:
 		_follow_input()
 	
 	move_and_collide(velocity * delta)
-
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if active_state.has_method("input"):
+		active_state.input(event)
 
 # Update velocity based on input
 func _follow_input() -> void:
