6e53a8370c67fb0a522a437029b0a121f23956af
claireejane <claireejane05@gmail.com>
2026-06-21 21:24:59 -0700

    bonmbokcatt

--- game/main.gd
+++ game/main.gd
@@ -10,7 +10,8 @@ extends Node2D
 @onready var ringo_R: AnimatableBody2D = $UI/Ringo2
 enum phase { PONG, SHOPL, SHOPR, BLACKJACK, PLINKO } #maybe add plinko phases and check these conditions for bugfixing?
 enum state { L_RECEIVE, L_EMIT, R_RECEIVE, R_EMIT }
-
+@onready var round = 1
+var minigame_number = 1 #0 is bj, 1 is plinko
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SaveData.current_phase = phase.PONG
@@ -28,15 +29,16 @@ func _on_game_game_won(player: String) -> void:
 	current.game_won.disconnect(_on_game_game_won)
 	remove_child.call_deferred(current)
 	#var minigame_options = [blackjack_scene, plinko_scene]
-	#var minigame = minigame_options.pick_random()
-	var minigame = plinko_scene
-	SaveData.current_phase = phase.PLINKO
-	#var minigame = blackjack_scene
-	current = minigame.instantiate()
-	add_child.call_deferred(current)
-	current.minigame_over.connect(_on_minigame_over)
-	current.balance_update.connect(balance_update)
-
+	#minigame_number = randi_range(0, 1)
+	#if minigame_number == 0: SaveData.current_phase = phase.BLACKJACK
+	#else: SaveData.current_phase = phase.PLINKO
+	#var minigame = minigame_options[0]
+	#var minigame = plinko_scene
+	#current = minigame.instantiate()
+	#add_child.call_deferred(current)
+	#current.minigame_over.connect(_on_minigame_over)
+	#current.balance_update.connect(balance_update)
+	_on_minigame_over()
 
 
 func _on_minigame_over() -> void:
@@ -45,6 +47,7 @@ func _on_minigame_over() -> void:
 	SaveData.current_phase = phase.SHOPL
 	current = shop.instantiate()
 	add_child.call_deferred(current)
+	
 
 func balance_update(player: String, amount: int) -> void:
 	print("updating " + player + "balance to " + str(amount))
--- game/ui.gd
+++ game/ui.gd
@@ -1,12 +1,38 @@
 class_name UI
 extends CanvasLayer
-
-
+@onready var transition: Control = $SceneTransition
+@onready var bottom_text: RichTextLabel = $SceneTransition/VBoxContainer/BottomText
+var accepted: bool = false
+signal switched(player: String)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	transition.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func switch_to(player:String) -> void:
+	match player:
+		"left":
+			bottom_text.set_text("Please press D to confirm that you are Player 1.")
+			transition.show()
+			await wait_for_D()
+			switched.emit(player)
+		"right":
+			bottom_text.set_text("Please press -> to confirm that you are Player 2.")
+			transition.show()
+			await wait_for_D()
+	
+func wait_for_D() -> void:
+	while true:
+		await get_tree().process_frame
+		if Input.is_action_just_pressed("left_attack"):
+			return
+
+func wait_for_forward() -> void:
+	while true:
+		await get_tree().process_frame
+		if Input.is_action_just_pressed("right_attack"):
+			return
